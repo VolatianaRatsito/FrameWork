09a084b4d40a5b8e4c56f22c8bd1dee5920aa33b
VolatianaRatsito <155939682+VolatianaRatsito@users.noreply.github.com>
2024-05-08 15:00:01 +0200

    Test

---
+++ Script.bat
@@ -0,0 +1,11 @@
+set url_controller=src\Controller
+set url_classes=classes
+set url_lib_test=..\Test\lib
+
+@REM compilation de FrontController
+javac -d "%url_classes%" "%url_controller%\*.java"
+
+@REM transforme de FrontController en .jar et envoye vers lib de Test
+jar -cvf "%url_lib_test%\class.jar" -C "%url_classes%" .
+
+pause
